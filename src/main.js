15fab164bad71d60752ad8b2ba1bf69f5718e8a1
pyropetrick <pavel.chernenko97@gmail.com>
2022-10-19 12:34:00 +0300

    TV: add vue bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+import "@babel/polyfill";
+import "mutationobserver-shim";
 import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import App from "./App.vue";
 import store from "./store";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
